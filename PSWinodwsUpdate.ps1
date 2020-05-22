e0744ed3ca81d360e5367bb12706401e7df245f6
Thorsten Enderlein <49591978+endoleg@users.noreply.github.com>
2020-05-22 16:32:44 +0200

    Update and rename psWinodwsupdate.ps1 to PSWinodwsUpdate.ps1

--- PSWinodwsUpdate.ps1
+++ PSWinodwsUpdate.ps1
@@ -5,6 +5,9 @@ Start-Transcript -Path "c:\Windows\Temp\PSWindowsUpdate.log" -Append
 # Set TLS to 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
+# Turn on updates for other Microsoft products
+(New-Object -ComObject Microsoft.Update.ServiceManager).AddService2("7971f918-a847-4430-9279-4a52d1efe18d", 7, "")
+
 write-verbose -message "NuGet install" -verbose
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 
@@ -18,6 +21,15 @@ write-verbose -message "list commands" -verbose
 get-command -module PsWindowsUpdate
 
 write-verbose -message "test only" -verbose
+Get-WUHistory
+
+write-verbose -message "Nach Updates suchen" -verbose
+Get-WindowsUpdate 
+
+write-verbose -message "Alle Updates installieren" -verbose
+Get-WindowsUpdate -AcceptAll -Install -MicrosoftUpdate -IgnoreReboot
+
+write-verbose -message "nur zum testen - Überblick" -verbose
 Get-WUHistory
 
 Stop-transcript
