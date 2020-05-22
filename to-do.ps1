ec165369d6d42148fa310f1e742ba15887e8d6b0
Thorsten Enderlein <49591978+endoleg@users.noreply.github.com>
2020-05-22 21:51:30 +0200

    Update to-do.ps1

--- to-do.ps1
+++ to-do.ps1
@@ -3,3 +3,8 @@
 # profile-backup script
 
 # win10hardening script
+
+# https://github.com/microsoft/winget-cli/blob/master/doc/windows-package-manager-v1-roadmap.md
+
+# https://winstall.app/store
+
