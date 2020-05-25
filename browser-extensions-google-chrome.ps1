ae2e9f60061f060a12133fa35083eb58fd598601
Thorsten Enderlein <49591978+endoleg@users.noreply.github.com>
2020-05-25 10:15:47 +0200

    Update browser-extensions-google-chrome.ps1

--- browser-extensions-google-chrome.ps1
+++ browser-extensions-google-chrome.ps1
@@ -102,3 +102,26 @@ else {
         Write-Host -ForegroundColor Red "Failed to create registry key"   
     }
 }
+
+
+
+
+<#
+#### ========================
+#### Install uBlock Plugin
+#### ------------------------
+function Remove-ChromeuBlock
+{
+	Remove-ItemProperty -Force -Path "HKCU:\Software\Google\Chrome\PreferenceMACs\Default\extensions.settings\" -Name "cjpalhdlnbpafiamejdnhcphjbkeiagm"
+	#### Remove-ItemProperty -Force -Path "HKLM:\Software\Policies\Google\Chrome\" -Name "ExtensionInstallForcelist"
+	Remove-Item -Force -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\cjpalhdlnbpafiamejdnhcphjbkeiagm*" -Recurse
+}
+function Install-ChromeuBlock
+{
+	New-Item -Force -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\cjpalhdlnbpafiamejdnhcphjbkeiagm"
+	New-ItemProperty -Force -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\cjpalhdlnbpafiamejdnhcphjbkeiagm\" -Name "update_url" -Value "https://clients2.google.com/service/update2/crx"
+	New-ItemProperty -Force -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\cjpalhdlnbpafiamejdnhcphjbkeiagm\" -Name "uBlock-Forced" -Value "oblockorigin-chrome"
+}
+Install-ChromeuBlock
+#### ========================
+#>
