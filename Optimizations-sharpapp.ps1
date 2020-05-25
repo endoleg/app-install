9e7e768f7fa7642dcb337f6c98b715a96940ee2c
Thorsten Enderlein <49591978+endoleg@users.noreply.github.com>
2020-05-25 08:38:56 +0200

    Update Optimizations-sharpapp.ps1

--- Optimizations-sharpapp.ps1
+++ Optimizations-sharpapp.ps1
@@ -1,9 +1,7 @@
-# https://github.com/mirinsoft/sharpapp
-
-# Download latest release from github
+write-verbose -message "Download latest sharpapp release from github - https://github.com/mirinsoft/sharpapp" -verbose
 $Repo = "https://api.github.com/repos/mirinsoft/sharpapp/releases/latest"
 
-# --- Query the API to get the url of the zip
+write-verbose -message "Query the API to get the url of the zip" -verbose
 $APIResponse = Invoke-RestMethod -Method Get -Uri $Repo
 $APIResponse
 $FileUrl = $APIResponse.assets.browser_download_url
@@ -11,8 +9,7 @@ $FileUrl
 
 $downloadFile = "C:\Windows\Temp\sharpapp.zip"
 
-# Let the download begin!
-Write-Output “Starting download”
+write-verbose -message "Download" -verbose
 $webClient = New-Object System.Net.WebClient
 $webClient.DownloadFile(“$FileUrl” ,
                         $downloadFile)
@@ -21,6 +18,7 @@ Write-Output “Downloaded to $downloadFile”
 Write-Output “extract”
 Expand-Archive $downloadFile -DestinationPath C:\Windows\Temp\ -Force
 
+write-verbose -message "starting sharpapp" -verbose
 cd C:\Windows\Temp\
 Start-Process C:\Windows\Temp\sharpapp.exe
 
