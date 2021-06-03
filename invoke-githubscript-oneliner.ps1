6ec81511bd4932fdb4342fd967e1c42173b04954
Thorsten Enderlein <49591978+endoleg@users.noreply.github.com>
2021-06-03 19:48:17 +0200

    Update invoke-githubscript-oneliner.ps1

--- invoke-githubscript-oneliner.ps1
+++ invoke-githubscript-oneliner.ps1
@@ -1,3 +1,3 @@
 # Just 3 Lines of native Powershell to install some apps
 # Usage:
-powershell -nop -exec bypass -c "IEX (New-Object Net.WebClient).DownloadString('http://')"
+powershell -nop -exec bypass -c "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/endoleg/app-install/master/chocolatey-remote.ps1')"
