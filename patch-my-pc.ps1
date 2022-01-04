65dd385d0a558690aecabbaf4edeb56ba81b8946
Thorsten Enderlein <49591978+endoleg@users.noreply.github.com>
2022-01-04 23:51:24 +0100

    Create patch-my-pc.ps1

---
+++ patch-my-pc.ps1
@@ -0,0 +1,72 @@
+Clear-Host
+
+Write-Verbose "Setting Arguments" -Verbose
+$ProgressPreference = 'SilentlyContinue'
+#$Icons = "C:\Icons"
+$Path = "C:\Windows\Temp"
+$Template = "$Path\AppV_Template.appvt"
+$TemplateURL = "https://raw.githubusercontent.com/haavarstein/Applications/master/AppV_Template.appvt"
+$XML = "$Path\Applications.xml"
+$XMLURL = "https://raw.githubusercontent.com/haavarstein/Applications/master/Applications.xml"
+$PatchMyPC = "$Path\Definitions.xml"
+$PatchMyPCURL = "https://patchmypc.com/freeupdater/definitions/definitions.xml"
+$TeamsWebHook = "XXXXXXXXXXXXX"
+
+# Set Timeout
+[System.Net.ServicePointManager]::MaxServicePointIdleTime = 5000000
+
+Write-Verbose "Installing Required PowerShell Modules" -Verbose
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+if (!(Test-Path -Path "C:\Program Files\PackageManagement\ProviderAssemblies\nuget")) { Install-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies }
+if (!(Get-Module -ListAvailable -Name Evergreen)) { Install-Module Evergreen -Force | Import-Module Evergreen }
+#if (!(Get-Module -ListAvailable -Name IntuneWin32App)) {Install-Module IntuneWin32App -Force | Import-Module IntuneWin32App}
+#Install-Module powershell-yaml
+If (!(Test-Path -Path $Path)) {New-Item -ItemType directory -Path $Path | Out-Null}
+Invoke-WebRequest -UseBasicParsing -Uri $TemplateURL -OutFile $Template
+Invoke-WebRequest -UseBasicParsing -Uri $XMLURL -OutFile $XML
+Invoke-WebRequest -UseBasicParsing -Uri $PatchMyPCURL -OutFile $PatchMyPC
+
+$MyConfigFileloc = ("$XML")
+[xml]$MyConfigFile = (Get-Content $MyConfigFileLoc)
+
+$MyDefinitionFileloc = ("$PathMyPC")
+[xml]$MyDefinitionFile = (Get-Content $Path\Definitions.xml)
+
+#$TentantID = "xenappblog.com"
+
+
+[xml]$MyDefinitionFile = (Get-Content $Path\Definitions.xml)
+
+#$MyDefinitionFile
+
+
+foreach ($App in $MyConfigFile.Applications.ChildNodes)
+{
+
+#Connect-MSIntuneGraph -TenantID $TentantID | Out-Null
+
+$Product = $App.Product
+$Product 
+$Vendor = $App.Vendor
+$Vendor
+$Architecture = $App.Architecture
+$Architecture
+$DisplayName = $App.DisplayName
+$DisplayName
+$PackageName = "$Product"
+$PackageName 
+$Evergreen = $App.Evergreen
+$Version = $MyDefinitionFile.Data.ARPData.$("$Product" + "Ver")
+$Version
+$URL = $MyDefinitionFile.Data.ARPData.$("$Product" + "Download")
+$URL 
+$InstallerType = $App.Installer
+$InstallerType 
+$UnattendedArgs = $App.Install
+$UnattendedArgs
+$UnattendedArgs = $UnattendedArgs.Replace("/i ","")
+$UnattendedArgs 
+$LogApp = "${env:SystemRoot}" + "\Temp\$Product $Version.log"
+$LogApp 
+write-output "-----------------------------------------------------------------"
+}
